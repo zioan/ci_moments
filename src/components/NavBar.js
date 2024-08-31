3f4661aba763a3e315b27348e7c8dc787ae4d97b
zioan <zaharia.ioan@gmail.com>
2024-08-31 12:19:55 +0200

    Using react router dom to create routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,32 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="Logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/" className={styles.NavLink}>
+          <Navbar.Brand>
+            <img src={logo} alt="Logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>
               <i className="fas fa-home"></i>
               Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
               <i className="fas fa-sign-in-alt"></i>
               Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
               <i className="fas fa-user-plus"></i>
               Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
